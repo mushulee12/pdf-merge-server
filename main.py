fd594e30251d62b89b914b80496a93a616646605
mushulee12 <nh2660001@gmail.com>
2025-07-17 11:05:01 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, send_file
+from PyPDF2 import PdfMerger
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/merge', methods=['POST'])
+def merge_pdf():
+    merger = PdfMerger()
+    files = request.files.getlist("pdfs")
+
+    for file in files:
+        path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(path)
+        merger.append(path)
+
+    output_path = os.path.join(UPLOAD_FOLDER, "merged.pdf")
+    merger.write(output_path)
+    merger.close()
+
+    return send_file(output_path, as_attachment=True)
